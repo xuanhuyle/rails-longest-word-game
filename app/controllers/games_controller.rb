86e50932a6280f2d63c6d4d21026d44f3f388d9f
xuan huy le <xuanhuyle@gmail.com>
2024-07-01 16:58:34 +0200

    word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,27 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
+    @word = params[:word]
+    @letters = params[:letters]
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+    if @included && @english_word
+      @points = params[:word].length.to_i
+    end
+  end
+
+private
+
+  def included?(word, letters)
+  end
+
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    JSON.parse(response.read)
   end
 end
